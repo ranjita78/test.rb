2a104933dd0e36072221ec14770cbdff6493e360
ranjita78 <ranjeetaachary@gmail.com>
2021-05-07 12:36:12 +0530

    second commit

--- day5/hello.rb
+++ day5/hello.rb
@@ -1,1 +1,8 @@
-put "hello world"
+n=[10,5,2,1,6]
+min=n[0]
+5.times do |i|
+    if n[i]<min
+        min = n[i]
+    end
+    return n[i]
+end
--- test.rb
+++ test.rb
@@ -1,5 +1,8 @@
-n=[10,1,21,3,4]
+n=[10,5,2,1,6]
+min=n[0]
 5.times do |i|
-
-    puts n[i]
+    if n[i] < min
+        min = n[i]
+    end
+    return n[i]
 end
